29cc1170e3e4a233503158390255e67abb64ba26
Kristian Tamm <tammkristian@gmail.com>
2024-03-12 14:35:34 +0200

    MoviePage, Trailer

--- Frontend/cinema-app/src/Components/moviePage/MoviePage.js
+++ Frontend/cinema-app/src/Components/moviePage/MoviePage.js
@@ -1,32 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../../api/axiosConfig';
+import Trailer from '../trailer/Trailer';
+import './MoviePage.css';
 
 const MoviePage = () => {
     const { id } = useParams();
     const [movie, setMovie] = useState(null);
 
     useEffect(() => {
-    const fetchMovie = async () => {
-        try {
-            const response = await api.get(`/api/v1/movies/${id}`);
-            setMovie(response.data);
-        } catch (err) {
-            console.log(err);
-        }
-    };
+        const fetchMovie = async () => {
+            try {
+                const response = await api.get(`/api/v1/movies/${id}`);
+                setMovie(response.data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
 
-    fetchMovie();
+        fetchMovie();
     }, [id]);
 
     if (!movie) {
-    return <div>Loading...</div>;
+        return <div className='loading'>Loading...</div>;
     }
 
+
     return (
-    <div>
-        <h1>{movie.title}</h1>
-    </div>
+        <div className='movie-page'>
+            <div className='trailer-details'>
+                <div className='trailer'>
+                    <Trailer title={movie.title} />
+                </div>
+                <div className='movie-details'>
+                    <h1 className='movie-title'>{movie.title}</h1>
+                    <p className='movie-genres'>{movie.genres.join(', ')}</p>
+                    <p className='movie-year'>{movie.year}</p>
+                    <p className='movie-runtime'>{movie.runtime} mins</p>
+                    <p className='movie-director'>Director: {movie.director}</p>
+                    <p className='movie-actors'>Actors: {movie.actors.join(', ')}</p>
+                    <h4 className='movie-rating'>{movie.rating}</h4>
+                    <button className='choose-seat-button'>Choose seats</button>
+                </div>
+            </div>
+            <div className='plot-poster'>
+                <p className='movie-plot'>{movie.plot}</p>
+                <img src={movie.posterUrl}/>
+            </div>
+        </div>
     );
 };
 
---
+++ Frontend/cinema-app/src/Components/trailer/Trailer.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Trailer = ({ title }) => {
+  const [trailerUrl, setTrailerUrl] = useState('');
+
+  const fetchTrailer = async (title) => {
+    try {
+      const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          q: `${title} official trailer`,
+          key: 'AIzaSyAZvmHNhkY3JROlLEih2dDiyePCAMaFwts',
+          type: 'video',
+        },
+      });
+
+      const videoId = response.data.items[0].id.videoId;
+      const url = `https://www.youtube.com/embed/${videoId}`;
+      console.log(videoId);
+      setTrailerUrl(url);
+    } catch (error) {
+      console.error('Error fetching trailer:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTrailer(title);
+  }, []);
+
+  return (
+    <div>
+      {trailerUrl && (
+        <iframe
+          style={{ width: '560px', height: '340px' }}
+          src={trailerUrl}
+          title="YouTube video player"
+          allowFullScreen
+        ></iframe>
+      )}
+    </div>
+  );
+};
+
+export default Trailer;
